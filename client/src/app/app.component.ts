644aa5e6d897f503dd468853b0b485bf3aa06e1d
gbo96 <gbenothmen2@gmail.com>
2019-04-29 20:27:22 +0100

    adding loader

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  showLoadingIndicator = true;
+
+  constructor(private router: Router){
+    this.router.events.subscribe((routerEvent: Event) => {
+      if(routerEvent instanceof NavigationStart){
+        this.showLoadingIndicator = true;
+      }
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationError ||
+        routerEvent instanceof NavigationCancel){
+
+          this.showLoadingIndicator = false;
+        }
+    });
+  }
 }
